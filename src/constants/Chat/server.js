1e7820cb404a33ef0027e885d7d4d9abaceb7b6c
BurningHat12 <yashgohel395@gmail.com>
2024-01-01 15:48:16 +0530

    Initial Commit

---
+++ src/constants/Chat/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for chat messages
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data); // Broadcast the message to all connected clients
+  });
+
+  // Listen for disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
